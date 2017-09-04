a506e63d589b80a87bdd051f5a6ff6d23abaeb4f
max-ryzhov <max.ryzhov12@gmail.com>
2017-09-04 23:28:33 +0300

    Changes for index.js

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var romanTime = require('./roman-time');
 console.info(romanTime('09:15'));
 
 // Выведет 'N:N'
-console.info(romanTime('00:20'));
+console.info(romanTime('00:25'));
 
 // Выведет 'XXIII:LIX'
-console.info(romanTime('23:59'));
+console.info(romanTime('23:35'));
 
 // Выбросится ошибка [TypeError: Неверное время]
-console.info(romanTime('24:00'));
+console.info(romanTime('24:45'));
