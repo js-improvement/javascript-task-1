27aeb0c4a4707e0096f5d4d649b5d1e54a996c8a
Ivan Rodionov <vanya.rodionov@gmail.com>
2017-09-04 21:34:39 +0300

    One minute added

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 var romanTime = require('./roman-time');
 
 // Выведет 'IX:X'
-console.info(romanTime('09:10'));
+console.info(romanTime('09:11'));
 
 // Выведет 'N:N'
-console.info(romanTime('00:00'));
+console.info(romanTime('00:01'));
 
-// Выведет 'XXIII:LIX'
-console.info(romanTime('23:59'));
+// Выведет 'XXIII:LIVIII'
+console.info(romanTime('23:58'));
 
 // Выбросится ошибка [TypeError: Неверное время]
-console.info(romanTime('24:00'));
+console.info(romanTime('24:01'));
