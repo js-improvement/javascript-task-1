ab23e391cca9eaccbeed5adee4669f6138bf4b0b
rodionovivan <vanya.rodionov@gmail.com>
2017-09-15 00:40:10 +0300

    еще попытка

--- index.js
+++ index.js
@@ -6,69 +6,54 @@ function checkArabicTimeCorrect (arabicTime){
         return;
     }
     // noinspection JSAnnotator
-    if ((+arabicTime.charAt(0) * 10 + +arabicTime.charAt(1)) < 0 || (+arabicTime.charAt(0) * 10 + +arabicTime.charAt(1)) > 23 || (+arabicTime.charAt(3) * 10 + +arabicTime.charAt(4)) < 0 || (+arabicTime.charAt(3) * 10 + +arabicTime.charAt(4)) > 59 || !(arabicTime.charAt(2) == ':')) { //uexpected arabic time value or format
+    if ((+arabicTime.charAt(0) * 10 + +arabicTime.charAt(1)) < 0 ||
+        (+arabicTime.charAt(0) * 10 + +arabicTime.charAt(1)) > 23 ||
+        (+arabicTime.charAt(3) * 10 + +arabicTime.charAt(4)) < 0 ||
+        (+arabicTime.charAt(3) * 10 + +arabicTime.charAt(4)) > 59 ||
+        !(arabicTime.charAt(2) == ':')) { //uexpected arabic time value or format
         console.info('wrong time format, you fuckin idiot!');
         return;
     }
     return arabicTime;
 }
-function translateArabicToRoman(arabicNumber){
-    var romanNumber = '';
-    console.info(arabicNumber-arabicNumber%10);
-    switch ((arabicNumber-arabicNumber%10)) {
-        case 50:
-            romanNumber = romanNumber + 'L';
-            break;
-        case 40:
-            romanNumber = romanNumber + 'IL';
-            break;
-        default:
-            var i = (arabicNumber-arabicNumber%10)/10;//number od X'es counting
-            while (i > 0) {
-                romanNumber = romanNumber + 'X';
-                i--;
-            }
-            break;
-    }
-    switch (arabicNumber%10){
-        case 9:
-            romanNumber = romanNumber + 'IX';
-            break;
-        case 8:
-        case 7:
-        case 6:
-        case 5:
-            romanNumber = romanNumber + 'V';
-            var j = arabicNumber % 5; //number od I'es counting
-            while (j > 0) {
-                romanNumber = romanNumber + 'I';
-                j--;
-            }
-            break;
-        case 4:
-            romanNumber = romanNumber + 'IV';
-            break;
-        case 3:
-        case 2:
-        case 1:
-            var k = arabicNumber % 5; //number od I'es counting
-            while (k > 0) {
-                romanNumber = romanNumber + 'I';
-                k--;
-            }
-            break;
-    }
-    return romanNumber;
-}
-function countRomanTime (arabicTime){
+function newTranslateArabicToRoman(arabicTime){
     var romanTime = '';
-    romanTime = romanTime + translateArabicToRoman(+arabicTime.charAt(0) * 10 + +arabicTime.charAt(1))+':'+translateArabicToRoman(+arabicTime.charAt(3) * 10 + +arabicTime.charAt(4));
+    if (Number(arabicTime.charAt(0))===5) romanTime = romanTime + "L";
+
+    else if (Number(arabicTime.charAt(0))===4)
+        romanTime = romanTime + "XL";
+
+    else {for (var i=0;i<Number(arabicTime.charAt(0));i++) romanTime = romanTime + "X";}
+    if (Number(arabicTime.charAt(1)) === 9) romanTime = romanTime + "IX";
+
+
+    else if (Number(arabicTime.charAt(1))>=5){
+        romanTime = romanTime + "V";
+        for(var j=0;j<Number(arabicTime.charAt(1))-5;j++) romanTime = romanTime + "I";
+    }
+    else if (Number(arabicTime.charAt(1))===4){
+        romanTime = romanTime + "IV";
+    }
+    else for (var k=0;k<Number(arabicTime.charAt(1));k++) romanTime = romanTime + "I";
+    return romanTime;
+}
+
+
+
+
+function newCountRomanTime (arabicTime){
+    var romanTime = '';
+    romanTime = romanTime + newTranslateArabicToRoman(arabicTime.charAt(0) + arabicTime.charAt(1))+
+        ':'+newTranslateArabicToRoman(arabicTime.charAt(3) + arabicTime.charAt(4));
     return romanTime;
 }
 function romanTime(arabicTime){
-    return countRomanTime(checkArabicTimeCorrect(arabicTime));
+    return newCountRomanTime(checkArabicTimeCorrect(arabicTime));
 }
 console.info(romanTime('09:54'));
+
 console.info(romanTime('22:38'));
+
 console.info(romanTime('19:02'));
+
 
